cce64a529d0e821f76dbce8e07e5563174115703
semilore <oluwatosinigbayilola@gmail.com>
2022-06-10 03:20:27 -0700

    Code reconstructed

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@
 def uniq_add(my_list=[]):
     result = 0
     for i in range(0, len(my_list)):
-        if my_list[reesult] in my_list[my_list[result]:]:
+        if my_list[result] in my_list[my_list[result]:]:
             my_list.pop(result)
     return sum(my_list)
